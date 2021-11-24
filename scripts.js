77776d81b1671f5ade1b0d81ac7a53cf9ae595bf
KingClouderton6 <islandintheclouds@outlook.com>
2021-11-24 23:49:41 +0000

    Added a painting function to turn each div red

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 const sketchContainer = document.querySelector('.sketch-container');
 const createButton = document.querySelector('#createButton');
-const div = document.createElement('div');
+
 
 createGrid(64);
 
@@ -8,14 +8,28 @@ function createGrid(num){
    
 
     for(let i = 0; i < num * num; i++){
-       const div = document.createElement('div');
+      const div = document.createElement('div');
         div.classList.add('grid-box');
         div.style.width = `${480/num}px`;
         sketchContainer.appendChild(div);
     }
 
-    return;
+    paintGridRed();
 }
+
+
+
+
+function paintGridRed(){
+    const redGrid = document.querySelectorAll('div')
+
+    redGrid.forEach((div) => {
+        div.addEventListener('mouseenter', function (e) {
+            e.target.style.background = 'red';
+        });
+    });
+}
+
 
 
 
